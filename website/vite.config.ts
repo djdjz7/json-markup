9914d7a2a432075b1125f18c48b9e71f14f4dbcf
djdjz7 <djdjz7@icloud.com>
2023-12-01 16:47:33 +0800

    remove vite-plugin-cdn-import

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import importToCDN from "vite-plugin-cdn-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    importToCDN({
-      modules: [
-        {
-          name: "monaco-editor",
-          var: "monaco",
-          path: "https://cdn.jsdelivr.net/npm/monaco-editor@0.44.0/min/vs/editor/editor.main.min.js",
-        },
-      ],
-    }),
-  ],
-
-  build: {
-    rollupOptions: {
-      external: ["monaco-editor"],
-    },
-  },
+  plugins: [vue()],
 });
